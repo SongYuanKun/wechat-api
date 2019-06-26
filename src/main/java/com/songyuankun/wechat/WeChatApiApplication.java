c20cb32774b9f47fa2141f53d78c57df447949a3
songyuankun <songyuankun@huohua.cn>
2019-06-26 21:30:45 +0800

    init

---
+++ src/main/java/com/songyuankun/wechat/WeChatApiApplication.java
@@ -0,0 +1,16 @@
+package com.songyuankun.wechat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author songyuankun
+ */
+@SpringBootApplication
+public class WeChatApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeChatApiApplication.class, args);
+    }
+
+}
