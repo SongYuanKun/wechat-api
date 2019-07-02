2202a58c720e69ede310061d6888769520963e38
songyuankun <songyuankun@huohua.cn>
2019-07-02 20:45:27 +0800

    加预约功能

---
+++ src/main/java/com/songyuankun/wechat/request/CourseEnrollForm.java
@@ -0,0 +1,22 @@
+package com.songyuankun.wechat.request;
+
+import io.swagger.annotations.ApiModel;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.io.Serializable;
+
+/**
+ * @author songyuankun
+ */
+@ApiModel
+@Getter
+@Setter
+@ToString
+public class CourseEnrollForm implements Serializable {
+    private int courseId;
+    private int userId;
+    private String phone;
+
+}
