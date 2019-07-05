730af3e87146e1c00f607e6a980f4c6cc8eda669
songyuankun <songyuankun@huohua.cn>
2019-07-05 16:44:01 +0800

    改utf-8mb4

--- src/main/java/com/songyuankun/wechat/dao/Course.java
+++ src/main/java/com/songyuankun/wechat/dao/Course.java
@@ -24,15 +24,43 @@ public class Course implements Serializable {
     @Id
     @GeneratedValue
     private Integer id;
+
+    /**
+     * 课程名称
+     */
     @Column(name = "name")
     private String name;
+
+    /**
+     * 课程简介
+     */
     @Column(name = "description")
     private String description;
+    /**
+     * 课程状态
+     */
     @Column(name = "status")
     private Integer status;
+    /**
+     * 课程封面
+     */
     private String img;
+
+    /**
+     * 教师名称
+     */
     @Column(name = "teacher_name")
     private String teacherName;
+    /**
+     * 课程简介
+     */
     private String introduction;
+
+    /**
+     * 课程详细介绍
+     */
+    @Column(name = "content_html")
+    private String contentHtml;
+
 
 }
