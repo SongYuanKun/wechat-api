fba5f352fdaae050ae1c46b324838be719874333
songyuankun <songyuankun@huohua.cn>
2019-07-08 22:57:21 +0800

    上传

--- src/main/java/com/songyuankun/wechat/common/Response.java
+++ src/main/java/com/songyuankun/wechat/common/Response.java
@@ -15,5 +15,15 @@ import lombok.ToString;
 @ToString
 public class Response<T> {
     private Integer code;
+    private String message;
     private T data;
+
+    public Response() {
+    }
+
+    public Response(Integer code, String message, T data) {
+        this.code = code;
+        this.message = message;
+        this.data = data;
+    }
 }
