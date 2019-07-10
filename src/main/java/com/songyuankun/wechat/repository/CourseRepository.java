dd746907528301568dda3812bedbb5457b28a99a
songyuankun <songyuankun@huohua.cn>
2019-07-10 23:21:57 +0800

    增加课程编辑功能

--- src/main/java/com/songyuankun/wechat/repository/CourseRepository.java
+++ src/main/java/com/songyuankun/wechat/repository/CourseRepository.java
@@ -1,14 +1,18 @@
 package com.songyuankun.wechat.repository;
 
 import com.songyuankun.wechat.dao.Course;
+import com.songyuankun.wechat.request.update.CourseUpdateDetail;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-
-import java.util.List;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
 /**
  * @author songyuankun
  */
 public interface CourseRepository extends JpaRepository<Course, Integer>, JpaSpecificationExecutor<Course> {
 
+    @Modifying
+    @Query(value = "update Course c set c.teacherName= :#{#course.teacherName} ,c.introduction= :#{#course.introduction}  where c.id= :#{#course.id}")
+    Integer updateDetail(Course course);
 }
