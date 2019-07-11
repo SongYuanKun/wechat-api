c821affcfd6e727573e8aa4e6ac1092ad62e05be
songyuankun <songyuankun@huohua.cn>
2019-07-11 22:33:52 +0800

    删除代码

--- src/main/java/com/songyuankun/wechat/repository/CourseEnrollRepository.java
+++ src/main/java/com/songyuankun/wechat/repository/CourseEnrollRepository.java
@@ -1,11 +1,8 @@
 package com.songyuankun.wechat.repository;
 
-import com.songyuankun.wechat.dao.Course;
 import com.songyuankun.wechat.dao.CourseEnroll;
-import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-import org.springframework.data.jpa.repository.Query;
 
 /**
  * @author songyuankun
