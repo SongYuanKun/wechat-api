cad3caa466fcadb20e58ac1844d4937ce472143a
songyuankun <songyuankun@huohua.cn>
2019-07-12 20:12:45 +0800

    11111

---
+++ src/main/java/com/songyuankun/wechat/dao/AppointmentTimePoint.java
@@ -0,0 +1,37 @@
+package com.songyuankun.wechat.dao;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.hibernate.annotations.Proxy;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Date;
+
+/**
+ * 房间被预订的时间点
+ *
+ * @author songyuankun
+ */
+@Proxy(lazy = false)
+@Entity
+@Table(name = "appointment_time_point")
+@ToString
+@Getter
+@Setter
+public class AppointmentTimePoint implements Serializable {
+
+    @Id
+    @GeneratedValue
+    private Integer id;
+
+    @Column(name = "room_id")
+    private Integer roomId;
+
+    @Column(name = "time_point_id")
+    private Integer timePointId;
+
+    private Date day;
+
+}
