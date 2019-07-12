ccf464cb04be455f3e1f5c97ae3724a5bb3ed4fb
songyuankun <songyuankun@huohua.cn>
2019-07-12 20:12:35 +0800

    111

--- src/main/java/com/songyuankun/wechat/repository/RoomAppointmentRepository.java
+++ src/main/java/com/songyuankun/wechat/repository/RoomAppointmentRepository.java
@@ -4,9 +4,17 @@ import com.songyuankun.wechat.dao.RoomAppointment;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
+import java.util.Date;
+import java.util.List;
+
 /**
  * @author songyuankun
  */
 public interface RoomAppointmentRepository extends JpaRepository<RoomAppointment, Integer>, JpaSpecificationExecutor<RoomAppointment> {
+
+
+    List<RoomAppointment> queryAllByStartTimeBetween(Date startTime1, Date startTime2);
+
+    List<RoomAppointment> queryAllByEndTimeBetween(Date startTime1, Date startTime2);
 
 }
