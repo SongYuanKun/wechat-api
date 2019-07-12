e753e5acf407870e5c7e3ea44fed7fea3440e2e9
songyuankun <songyuankun@huohua.cn>
2019-07-12 20:13:08 +0800

    11111

--- src/main/java/com/songyuankun/wechat/repository/CourseRepository.java
+++ src/main/java/com/songyuankun/wechat/repository/CourseRepository.java
@@ -1,7 +1,6 @@
 package com.songyuankun.wechat.repository;
 
 import com.songyuankun.wechat.dao.Course;
-import com.songyuankun.wechat.request.update.CourseUpdateDetail;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Modifying;
