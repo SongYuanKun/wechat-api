f759964a1147857ed4d6605785cf4178763b492f
songyuankun <songyuankun@huohua.cn>
2019-08-15 16:28:21 +0800

    改东西

--- src/main/java/com/songyuankun/wechat/controller/admin/ArticleAdminController.java
+++ src/main/java/com/songyuankun/wechat/controller/admin/ArticleAdminController.java
@@ -16,7 +16,6 @@ import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
 import javax.transaction.Transactional;
-import java.util.List;
 
 /**
  * @author songyuankun
