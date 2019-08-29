687a7ba8758f445456f796481437dd972da45cb1
songyuankun <songyuankun@huohua.cn>
2019-08-29 14:57:10 +0800

    解决创建数据报错问题

--- src/main/java/com/songyuankun/wechat/controller/admin/CategoryAdminController.java
+++ src/main/java/com/songyuankun/wechat/controller/admin/CategoryAdminController.java
@@ -1,14 +1,19 @@
 package com.songyuankun.wechat.controller.admin;
 
+import com.songyuankun.wechat.common.DaoCommon;
 import com.songyuankun.wechat.common.Response;
 import com.songyuankun.wechat.common.ResponseUtils;
 import com.songyuankun.wechat.entity.Category;
 import com.songyuankun.wechat.repository.CategoryRepository;
+import com.songyuankun.wechat.request.ArticleForm;
 import io.swagger.annotations.Api;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.BeanUtils;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -26,6 +31,23 @@ public class CategoryAdminController {
 
     public CategoryAdminController(CategoryRepository categoryRepository) {
         this.categoryRepository = categoryRepository;
+    }
+
+    @PostMapping("saveOrUpdate")
+    public Category save(Authentication authentication, @RequestBody ArticleForm articleForm) {
+        Category category = new Category();
+        BeanUtils.copyProperties(articleForm, category);
+        if (category.getId() == null) {
+            DaoCommon.createDao(authentication, category);
+        }
+        categoryRepository.save(category);
+        return category;
+    }
+
+    @PostMapping("page")
+    public Page<Category> page(@RequestParam(required = false, defaultValue = "0") Integer pageNumber, @RequestParam(required = false, defaultValue = "10") Integer pageSize) {
+        Pageable pageable = PageRequest.of(pageNumber, pageSize);
+        return categoryRepository.findAll(pageable);
     }
 
 
