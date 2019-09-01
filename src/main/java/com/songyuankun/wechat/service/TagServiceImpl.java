d68aea25350cfd2c46b3b3b72c3e0543dda4544a
songyuankun <123839070@qq.com>
2019-09-01 11:16:48 +0800

    标签

--- src/main/java/com/songyuankun/wechat/service/TagServiceImpl.java
+++ src/main/java/com/songyuankun/wechat/service/TagServiceImpl.java
@@ -3,6 +3,8 @@ package com.songyuankun.wechat.service;
 
 import com.google.common.collect.Lists;
 import com.songyuankun.wechat.entity.Tag;
+import com.songyuankun.wechat.entity.TagLink;
+import com.songyuankun.wechat.repository.TagLinkRepository;
 import com.songyuankun.wechat.repository.TagRepository;
 import com.songyuankun.wechat.request.query.TagQuery;
 import org.apache.commons.lang3.StringUtils;
@@ -11,8 +13,10 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
+import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.Predicate;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * @author songyuankun
@@ -20,9 +24,11 @@ import java.util.List;
 @Service
 public class TagServiceImpl {
     private final TagRepository tagRepository;
+    private final TagLinkRepository tagLinkRepository;
 
-    public TagServiceImpl(TagRepository tagRepository) {
+    public TagServiceImpl(TagRepository tagRepository, TagLinkRepository tagLinkRepository) {
         this.tagRepository = tagRepository;
+        this.tagLinkRepository = tagLinkRepository;
     }
 
     public Page<Tag> findAllByQuery(TagQuery tagQuery) {
@@ -38,5 +44,17 @@ public class TagServiceImpl {
             Predicate[] p = new Predicate[list.size()];
             return criteriaBuilder.and(list.toArray(p));
         }, pageable);
+    }
+
+    public List<Tag> getTagsByArticleId(Integer articleId) {
+        List<Integer> idList = tagLinkRepository.findAllByArticleId(articleId).stream().map(TagLink::getTagId).collect(Collectors.toList());
+        return tagRepository.findAll(((root, criteriaQuery, criteriaBuilder) -> {
+            CriteriaBuilder.In<Integer> in = criteriaBuilder.in(root.get("id").as(Integer.class));
+            idList.forEach(in::value);
+            List<Predicate> list = Lists.newArrayList();
+            list.add(in);
+            Predicate[] p = new Predicate[list.size()];
+            return criteriaBuilder.and(list.toArray(p));
+        }));
     }
 }
