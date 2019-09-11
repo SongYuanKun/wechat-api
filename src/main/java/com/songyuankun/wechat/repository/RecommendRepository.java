1600bd9baec0bb788f23114a52fdd61e3a352268
songyuankun <songyuankun@huohua.cn>
2019-09-11 22:23:05 +0800

    recommend

--- src/main/java/com/songyuankun/wechat/repository/RecommendRepository.java
+++ src/main/java/com/songyuankun/wechat/repository/RecommendRepository.java
@@ -1,11 +1,14 @@
 package com.songyuankun.wechat.repository;
 
 import com.songyuankun.wechat.entity.Recommend;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 /**
  * @author songyuankun
