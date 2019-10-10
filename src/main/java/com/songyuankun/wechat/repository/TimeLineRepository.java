b7f316fe17f001db0c2ef14a0ba7a262f5523ef2
songyuankun <songyuankun@huohua.cn>
2019-10-10 21:05:29 +0800

    继续改bug

--- src/main/java/com/songyuankun/wechat/repository/TimeLineRepository.java
+++ src/main/java/com/songyuankun/wechat/repository/TimeLineRepository.java
@@ -15,6 +15,6 @@ public interface TimeLineRepository extends JpaRepository<Article, Integer> {
     @Query(value = " select DATE_FORMAT(create_time,'%Y') as year,count(DATE_FORMAT(create_time,'%Y')) as count from article where publish = 1 group by create_time", nativeQuery = true)
     List<Object[]> listTimeline();
 
-    @Query(value = "select id,title,description,create_time  from article where DATE_FORMAT(create_time,'%Y')=#{year} and DATE_FORMAT(create_time,'%m')=1 and publish = 1", nativeQuery = true)
+    @Query(value = "select id,title,description,create_time  from article where DATE_FORMAT(create_time,'%Y')=:year and DATE_FORMAT(create_time,'%m')=:month and publish = 1", nativeQuery = true)
     List<Object[]> listTimelinePost(@Param("year") Integer year, @Param("month") Integer month);
 }
