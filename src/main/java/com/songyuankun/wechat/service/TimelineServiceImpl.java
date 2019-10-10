cdb975efc06444132d9cff623483fa95efcd559f
songyuankun <songyuankun@qq.com>
2019-10-10 23:43:22 +0800

    1

--- src/main/java/com/songyuankun/wechat/service/TimelineServiceImpl.java
+++ src/main/java/com/songyuankun/wechat/service/TimelineServiceImpl.java
@@ -4,10 +4,11 @@ import com.songyuankun.wechat.entity.Timeline;
 import com.songyuankun.wechat.entity.TimelineMonth;
 import com.songyuankun.wechat.entity.TimelinePost;
 import com.songyuankun.wechat.repository.TimeLineRepository;
+import com.songyuankun.wechat.util.DateUtil;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.util.CollectionUtils;
 
-import java.sql.Date;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.List;
@@ -16,6 +17,7 @@ import java.util.List;
  * @author songyuankun
  */
 @Service
+@Slf4j
 public class TimelineServiceImpl {
 
     private final TimeLineRepository timeLineRepository;
@@ -37,9 +39,9 @@ public class TimelineServiceImpl {
         for (Timeline timeline : timelineList) {
             List<TimelineMonth> timelineMonthList = new ArrayList<>();
             for (int i = Calendar.DECEMBER + 1; i > 0; i--) {
-                List<Object[]> objects = timeLineRepository.listTimelinePost(timeline.getYear(), 9);
+                List<Object[]> objects = timeLineRepository.listTimelinePost(timeline.getYear(), i);
                 List<TimelinePost> postList = new ArrayList<>();
-                objects.forEach(objects1 -> postList.add(new TimelinePost(Integer.parseInt(objects1[0].toString()), objects1[1].toString(), objects1[2].toString(), Date.valueOf(objects1[3].toString()))));
+                objects.forEach(objects1 -> postList.add(new TimelinePost(Integer.parseInt(objects1[0].toString()), objects1[1].toString(), objects1[2].toString(), DateUtil.String2Date(objects1[3].toString()))));
 
                 if (CollectionUtils.isEmpty(postList)) {
                     continue;
