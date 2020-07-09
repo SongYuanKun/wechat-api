113ce6e3eb0503e5f510ea74da2336e8a9c4362d
songyuankun <songyuankun@huohua.cn>
2020-07-09 19:35:31 +0800

    多线程测试

---
+++ src/test/java/com/songyuankun/wechat/MyThreadTest.java
@@ -0,0 +1,26 @@
+package com.songyuankun.wechat;
+
+import lombok.extern.slf4j.Slf4j;
+import org.databene.contiperf.PerfTest;
+import org.databene.contiperf.junit.ContiPerfRule;
+import org.junit.Rule;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+
+@Slf4j
+public class MyThreadTest extends WeChatApiApplicationTests {
+
+    @Rule
+    public ContiPerfRule contiPerfRule = new ContiPerfRule();
+    @Autowired
+    private MyThread myThread;
+
+    @Test
+    //5个线程 执行100000次
+    @PerfTest(invocations = 100000, threads = 5)
+    public void test() {
+        myThread.get();
+    }
+
+}
