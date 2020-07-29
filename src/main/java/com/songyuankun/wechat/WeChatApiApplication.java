dcfb655f834348092021d74802539953ea3ef197
songyuankun <songyuankun@huohua.cn>
2020-07-30 01:11:45 +0800

    ···

--- src/main/java/com/songyuankun/wechat/WeChatApiApplication.java
+++ src/main/java/com/songyuankun/wechat/WeChatApiApplication.java
@@ -1,14 +1,14 @@
 package com.songyuankun.wechat;
 
-import com.alibaba.nacos.spring.context.annotation.config.NacosPropertySource;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * @author songyuankun
  */
 @SpringBootApplication
-@NacosPropertySource(dataId = "wechat-api", autoRefreshed = true)
+@EnableDiscoveryClient
 public class WeChatApiApplication {
 
     public static void main(String[] args) {
