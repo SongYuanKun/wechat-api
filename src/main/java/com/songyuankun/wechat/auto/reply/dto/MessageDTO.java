ad409dea265ab23ed3eb333fd1a8280b0f59d910
songyuankun <songyuankun@didiglobal.com>
2022-06-20 11:45:58 +0800

    clear
    Change-Id: Iaf0f0f38c641924ab354a74e2d750d081063d5f9

--- src/main/java/com/songyuankun/wechat/auto/reply/dto/MessageDTO.java
+++ src/main/java/com/songyuankun/wechat/auto/reply/dto/MessageDTO.java
@@ -9,32 +9,32 @@ import javax.xml.bind.annotation.XmlRootElement;
 
 @Data
 @ToString
-@XmlRootElement(name = "xml" )
+@XmlRootElement(name = "xml")
 public class MessageDTO {
 
     /**
      * 开发者微信号
      */
-    @XmlElement(name = "ToUserName" )
+    @XmlElement(name = "ToUserName")
     private String toUserName;
     /**
      * 发送方帐号（一个OpenID）
      */
-    @XmlElement(name = "FromUserName" )
+    @XmlElement(name = "FromUserName")
     private String fromUserName;
     /**
      * 消息创建时间 （整型）
      */
-    @XmlElement(name = "CreateTime" )
+    @XmlElement(name = "CreateTime")
     private String createTime;
     /**
      * 消息类型，文本为text
      */
-    @XmlElement(name = "MsgType" )
+    @XmlElement(name = "MsgType")
     private String msgType;
     /**
      * 文本消息内容
      */
-    @XmlElement(name = "Content" )
+    @XmlElement(name = "Content")
     private String content;
 }
