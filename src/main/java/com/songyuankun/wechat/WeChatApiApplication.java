5ee5c9d93041128340804eda11ee27ea8ee40e5a
songyuankun <123839070@QQ.COM>
2022-10-01 10:47:23 +0800

    remove spring cloud  dependency

--- src/main/java/com/songyuankun/wechat/WeChatApiApplication.java
+++ src/main/java/com/songyuankun/wechat/WeChatApiApplication.java
@@ -2,13 +2,11 @@ package com.songyuankun.wechat;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * @author songyuankun
  */
 @SpringBootApplication
-@EnableDiscoveryClient
 public class WeChatApiApplication {
     public static void main(String[] args) {
         SpringApplication.run(WeChatApiApplication.class, args);
