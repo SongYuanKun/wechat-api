d7a11752d4e8a63caceab846e239c56d8eb582fe
songyuankun <123839070@qq.com>
2023-03-17 17:47:53 +0800

    change gradle

--- src/main/java/com/songyuankun/wechat/blog/application/assembler/ArticleMapper.java
+++ src/main/java/com/songyuankun/wechat/blog/application/assembler/ArticleMapper.java
@@ -2,12 +2,53 @@ package com.songyuankun.wechat.blog.application.assembler;
 
 import com.songyuankun.wechat.blog.domain.aggregate.Article;
 import com.songyuankun.wechat.infrastructure.dataaccess.ArticlePO;
-import org.mapstruct.Mapper;
 
-@Mapper(componentModel = "spring")
-public interface ArticleMapper {
-    ArticlePO toArticlePO(Article article);
+import org.springframework.stereotype.Component;
 
-    Article toArticle(ArticlePO articlePO);
+@Component
+public class ArticleMapper {
+
+    public ArticlePO toArticlePO(Article article) {
+        return ArticlePO.builder()
+            .id(article.getId())
+            .mediaId(article.getMediaId())
+            .title(article.getTitle())
+            .description(article.getDescription())
+            .author(article.getAuthor())
+            .content(article.getContent())
+            .readNum(article.getReadNum())
+            .likeNum(article.getLikeNum())
+            .cover(article.getCover())
+            .thumbMediaId(article.getThumbMediaId())
+            .coverType(article.getCoverType())
+            .recommend(article.getRecommend())
+            .categoryId(article.getCategoryId())
+            .publish(article.getPublish())
+            .top(article.getTop())
+            .build();
+
+    }
+
+    public Article toArticle(ArticlePO articlePO) {
+        return Article.builder()
+            .id(articlePO.getId())
+            .mediaId(articlePO.getMediaId())
+            .title(articlePO.getTitle())
+            .description(articlePO.getDescription())
+            .author(articlePO.getAuthor())
+            .content(articlePO.getContent())
+            .readNum(articlePO.getReadNum())
+            .likeNum(articlePO.getLikeNum())
+            .cover(articlePO.getCover())
+            .thumbMediaId(articlePO.getThumbMediaId())
+            .coverType(articlePO.getCoverType())
+            .recommend(articlePO.getRecommend())
+            .categoryId(articlePO.getCategoryId())
+            .publish(articlePO.getPublish())
+            .top(articlePO.getTop())
+            .build();
+
+
+    }
 
 }
